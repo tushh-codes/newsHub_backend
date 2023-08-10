6c085072675adc0941ae46e9d7d10ca72a6c382d
Tushh <tushar2643@gmail.com>
2023-08-10 23:19:59 +0530

    changes

--- index.js
+++ index.js
@@ -66,6 +66,8 @@ app.post('/register', (req, res) => {
     });
 });
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log('listening on port 5000!');
 });
